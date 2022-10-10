4c811b9a8ad611276644edeb06deafa597251828
Hhhhhhhao <haoc3@andrew.cmu.edu>
2022-10-10 13:33:34 -0400

    optimizer configs

---
+++ configs_hfai/resnet/resnet50_8xb256_ep300_wep20_lamb_coslr_in1k.py
@@ -0,0 +1,22 @@
+_base_ = [
+    './models/resnet50.py',
+    './datasets/imagenet_bs64.py',
+    './schedules/imagenet_bs2048_lamb_coslr_ep300.py',
+    './runtimes/default_runtime.py'
+]
+
+
+data = dict(
+    samples_per_gpu=256,
+    workers_per_gpu=4
+)
+
+
+optimizer = dict(type='Lamb', lr=0.005, weight_decay=0.02)
+lr_config = dict(
+    policy='CosineAnnealing',
+    min_lr=1.0e-6,
+    warmup='linear',
+    # For ImageNet-1k, 626 iters per epoch, warmup 5 epochs.
+    warmup_iters=20 * 626,
+    warmup_ratio=0.0001)
